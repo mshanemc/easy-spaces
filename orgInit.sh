f1799ac58454838416b1c6a47b482763c0aa78e2
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2018-10-12 15:53:43 -0500

    no packages, open to theme page

--- orgInit.sh
+++ orgInit.sh
@@ -1,8 +1,4 @@
 sfdx force:org:create -s -f config/project-scratch-def.json -d 1
-sfdx force:package:install --package es-base-objects@1.0.0-1 -w 20
-sfdx force:package:install --package es-base-code@1.0.0-1 -w 20
-sfdx force:package:install --package es-base-styles@1.0.0-1 -w 20
-sfdx force:package:install --package es-space-mgmt@1.0.0-1 -w 20
 sfdx force:source:push
 
 sfdx force:user:permset:assign -n EasySpacesObjects
@@ -11,4 +7,4 @@ sfdx force:user:permset:assign -n SpaceManagementApp
 sfdx force:data:tree:import --plan ./data/Plan1.json
 sfdx force:data:tree:import --plan ./data/Plan2.json
 
-sfdx force:org:open
+sfdx force:org:open -p /lightning/setup/ThemingAndBranding/home
